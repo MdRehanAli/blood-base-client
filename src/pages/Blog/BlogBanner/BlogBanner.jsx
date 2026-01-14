abb61ab0eb65c390e66004247b79e308b36afe91
MdRehanAli <rehanali21d@gmail.com>
2026-01-15 00:27:21 +0600

    Update Blogs

--- src/pages/Blog/BlogBanner/BlogBanner.jsx
+++ src/pages/Blog/BlogBanner/BlogBanner.jsx
@@ -6,7 +6,7 @@ const BlogBanner = () => {
     return (
         <div className='bg-cover bg-center' style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.7)), url(${bg})` }}>
             <div className=" text-white py-30 rounded-2xl max-w-7xl mx-auto w-11/12">
-                <h1 className='text-center text-2xl md:text-5xl font-bold text-red-600'>Blog</h1>
+                <h1 className='text-center text-2xl md:text-5xl font-bold text-red-600'>Blogs</h1>
                 <p className='my-5 text-red-400 text-center'>Donate, Save & Inspire</p>
                 <p className='text-center text-red-100 hidden lg:block'>Blood Base is a compassionate and user-friendly single-page web application built to connect blood donors, recipients, and organizers of donation events. It serves as a digital bridge for saving lives, making the process of finding, registering, and donating blood easier and more impactful.</p>
                 <div className='flex justify-center'>
